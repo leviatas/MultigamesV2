f01164621d1aff41bec30c77afcf4c98523148a3
Peluffo Eduardo <ePeluffo@cas.austral.edu.ar>
2023-01-25 12:01:06 -0300

    FIX: Error al mandarm mensaje de nueva version

--- MainController.py
+++ MainController.py
@@ -792,7 +792,7 @@ def main():
 	# log all errors
 	dp.add_error_handler(error)
 
-	updater.bot.send_message(ADMIN, "Nueva version en linea")
+	updater.bot.send_message(ADMIN[0], "Nueva version en linea")
 
 	# Start the Bot (Usar si no es WebHook)
 	updater.start_polling(timeout=30)
