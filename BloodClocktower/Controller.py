73f2cf987720fede793e5eb2cf1ea535de819c88
Peluffo Eduardo <ePeluffo@cas.austral.edu.ar>
2023-02-28 10:31:58 -0300

    Blood: Add travel cmd to controller

--- BloodClocktower/Controller.py
+++ BloodClocktower/Controller.py
@@ -110,6 +110,7 @@ def main():
     dp.add_handler(CommandHandler("call", Commands.command_call))
 
     dp.add_handler(CommandHandler("id", Commands.command_id))
+    dp.add_handler(CommandHandler("travel", Commands.command_travel))
 
     dp.add_handler(CallbackQueryHandler(pattern=r"(-[0-9]*)\*choosegameblood\*(.*)\*([0-9]*)", callback=Commands.callback_choose_game_blood))
 
